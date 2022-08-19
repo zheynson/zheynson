92a5c6ebac509af9931b68d7942ba64fcb84bb24
zheynson <zheynson@gmail.com>
2022-08-19 13:00:51 +0300

    First - 8

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 const nav = document.querySelector('#nav');
 const navBtn = document.querySelector('#nav-btn');
 const navBtnImg = document.querySelector('#nav-btn-img');
+const nav_li = document.getElementsByClassName('nav-li')
+
+nav_li.onclick = () => {
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = './assets/nav-close.svg';
+    }else {
+        navBtnImg.src = './assets/nav.svg';
+    }
+}
 
 navBtn.onclick = () => {
     if (nav.classList.toggle('open')) {
@@ -10,6 +19,17 @@ navBtn.onclick = () => {
     }
 }
 
+// ANIMATION
 AOS.init({
     once: true
+});
+
+// SCROLL
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
 });
