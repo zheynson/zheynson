fd929678fa716de41c04bc78f1f51296fe72fb3b
zheynson <zheynson@gmail.com>
2022-09-15 20:39:30 +0300

    First - 11

--- js/script.js
+++ js/script.js
@@ -5,6 +5,18 @@ const navBtnImg = document.querySelector('#nav-btn-img');
 const navClose = document.querySelector('.close_window_1');
 const navCloseTwo = document.querySelector('.close_window_2');
 const navCloseThree = document.querySelector('.close_window_3');
+const navBtnPlus = document.querySelector('.nav-button-plus');
+
+const headerNavPlus = document.querySelector('.header-nav-plus');
+
+navBtnPlus.onclick = () => {
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = './assets/nav-close.svg';
+        headerNavPlus.classList.add('nav-list')
+    }else {
+        navBtnImg.src = './assets/nav.svg';
+    }
+}
 
 navCloseTwo.onclick = () => {
     if (nav.classList.toggle('open')) {
@@ -49,11 +61,11 @@ $(document).ready(function(){
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
+         $('body,html').animate({scrollTop: top}, 1500); //1500
     });
 });
 
-// HEADING-SCROLL
+// HEADING-bubble
 var $header = $(".header-nav-plus")
 var scroll = 75;
 var active = "active"
